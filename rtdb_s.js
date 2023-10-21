ea639a7216a2f5571cfd299a02953d82432ec179
Nguyen Luong Duy <33522847+ngylduy@users.noreply.github.com>
2023-10-21 14:03:50 +0700

    Update rtdb_s.js

--- rtdb_s.js
+++ rtdb_s.js
@@ -8,7 +8,7 @@ import {
     get as s,
     update as i
 } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js";
-let firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyBcXhv-UL1Bhp_KjvvkRVFTTPNmYp5ytI0",
     authDomain: "shortlink-ebf23.firebaseapp.com",
     databaseURL: "https://shortlink-ebf23-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -16,47 +16,47 @@ let firebaseConfig = {
     storageBucket: "shortlink-ebf23.appspot.com",
     messagingSenderId: "1073112615716",
     appId: "1:1073112615716:web:8f1c3032847c33613e01b1"
-}, app = t(firebaseConfig), db = e(), fRtdb = async () => {
-    let t = sessionStorage.getItem("_shortL");
+}, app = t(firebaseConfig), db = e(), fRtdbs = async () => {
+    const t = sessionStorage.getItem("_shortL");
     if (t) {
-        let e = JSON.parse(t),
-            r = "stu/" + e.by + "/" + e.id + "/";
-        if (await s(c(a(db), r + "ori")).then(t => !!t.exists() && t.val() == e.ori).catch(() => !1)) {
-            let l = new Date,
-                b = l.getFullYear(),
-                p = String(l.getMonth() + 1).padStart(2, "0"),
-                n = String(l.getDate()).padStart(2, "0"),
-                o = [b, p].join("-"),
-                _ = await s(c(a(db), "userLevel/" + e.by + "/level")).then(t => t.exists() && t.val()).catch(() => !1);
-            _ && (_ = await s(c(a(db), "config/stu/level/" + _ + "/cpc")).then(t => t.exists() ? t.val() : 1));
-            let h = await s(c(a(db), "config/stu/cpc")).then(t => t.exists() ? t.val() : 1),
-                d = _ || h;
-            document.querySelectorAll(".icdbx").forEach(t => {
-                t.addEventListener("click", async () => {
+        const c = JSON.parse(t),
+            e = "stu/" + c.by + "/" + c.id + "/";
+        if (await get(child(ref(db), e + "ori")).then((t => !!t.exists() && t.val() == c.ori)).catch((() => !1))) {
+            const t = new Date,
+                a = t.getFullYear(),
+                i = String(t.getMonth() + 1).padStart(2, "0"),
+                s = String(t.getDate()).padStart(2, "0"),
+                l = [a, i].join("-");
+            let n = await get(child(ref(db), "userLevel/" + c.by + "/level")).then((t => t.exists() && t.val())).catch((() => !1));
+            n && (n = await get(child(ref(db), "config/stu/level/" + n + "/cpc")).then((t => t.exists() ? t.val() : 1)));
+            const r = await get(child(ref(db), "config/stu/cpc")).then((t => t.exists() ? t.val() : 1)),
+                d = n || r;
+            document.querySelectorAll(".icdbx").forEach((t => {
+                t.addEventListener("click", (async () => {
                     if (stVC() && t.classList.contains("a")) {
-                        let l = await s(c(a(db), r + "click")).then(t => t.exists() ? t.val() + 1 : 1).catch(() => 1),
-                            b = await s(c(a(db), "/reports/" + e.by + "/" + o + "/" + n)).then(t => {
-                                let e = t.val();
+                        const t = await get(child(ref(db), e + "click")).then((t => t.exists() ? t.val() + 1 : 1)).catch((() => 1)),
+                            a = await get(child(ref(db), "/reports/" + c.by + "/" + l + "/" + s)).then((t => {
+                                let c = t.val();
                                 return {
-                                    click: e.click + 1,
-                                    cpc: (e.click * e.cpc + d) / (e.click + 1)
+                                    click: c.click + 1,
+                                    cpc: (c.click * c.cpc + d) / (c.click + 1)
                                 }
-                            }).catch(() => ({
+                            })).catch((() => ({
                                 click: 1,
                                 cpc: d
-                            })),
-                            p = await s(c(a(db), "/balance/" + e.by + "/current")).then(t => t.val() + d).catch(() => d),
-                            _ = {
-                                ["/" + r + "click"]: l,
-                                ["/reports/" + e.by + "/" + o + "/" + n + "/click"]: b.click,
-                                ["/reports/" + e.by + "/" + o + "/" + n + "/cpc"]: b.cpc,
-                                ["/balance/" + e.by + "/current"]: p
+                            }))),
+                            i = await get(child(ref(db), "/balance/" + c.by + "/current")).then((t => t.val() + d)).catch((() => d)),
+                            n = {
+                                ["/" + e + "click"]: t,
+                                ["/reports/" + c.by + "/" + l + "/" + s + "/click"]: a.click,
+                                ["/reports/" + c.by + "/" + l + "/" + s + "/cpc"]: a.cpc,
+                                ["/balance/" + c.by + "/current"]: i
                             };
-                        i(a(db), _).then(() => inVC()).catch(t => t.message)
+                        update(ref(db), n).then((() => inVC())).catch((t => t.message))
                     }
-                })
-            })
+                }))
+            }))
         }
     }
 };
-fRtdb();
+fRtdbs();
